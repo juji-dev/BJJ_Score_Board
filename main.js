bdf1eafcf997f2a97e0bb97f291e2a9b6e12cf47
josephbreslin <c15713979@mydit.ie>
2019-08-04 18:56:25 +0100

    Some updates to scoreboard.index

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+const path = require('path')
+
+let win;
+var formData;
 
 function createWindow () {
   // Create the browser window.
-  let win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1000,
     height: 800,
     webPreferences: {
@@ -10,8 +14,31 @@ function createWindow () {
     }
   })
 
+  //const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+ // Menu.setApplicationMenu(mainMenu)
   // and load the index.html of the app.
   win.loadFile('index.html')
 }
 
+//Catch Data
+ipcMain.on('data:add', function(event, data){
+  const fs = require('fs');
+  let formData = JSON.stringify(data);
+  fs.writeFileSync('data.json', formData)
+  win.loadFile('scoreBoard.html') 
+});
+
+const mainMenuTemplate = [{
+  label:'file'
+}];
+
 app.on('ready', createWindow)
+
+//if on mac
+if(process.platform =='darwin'){mainMenuTemplate.unshift({});}
+
+function populate()
+{
+  console.log(formData)
+  document.getElementById("body").innerHTML = formData;
+}
